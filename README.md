bcf989cf312812ea76a4e9ccb934fc35a2b902a4
M-irfan <fannee0011@gmail.com>
2026-07-11 11:29:05 +0500

    printing statements

--- README.md
+++
@@ -1,2 +0,0 @@
-# go-language
-# go-language
---
+++ hello-world/printing.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("engineer + coder = programmer")
+	fmt.Println(123)
+	fmt.Println(123 + 23)
+
+}
