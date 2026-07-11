eb76cb47c21b36f2fa4334a1e3c75f00d8c1f7ba
M-irfan <fannee0011@gmail.com>
2026-07-11 12:04:09 +0500

    printing simple math values

---
+++ simple-values/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//strings
+	fmt.Println("hello world")
+	fmt.Println("hello world + 123")
+
+	//number
+	fmt.Println(123)
+
+	//float
+	fmt.Println(2.4)
+	fmt.Println(88.94)
+
+	//bool
+	fmt.Println(true)
+	fmt.Println(false)
+
+	//simple math
+	fmt.Println(3 + 4)
+	fmt.Println(3 - 88)
+	fmt.Println(3 / 4)
+	fmt.Println(3 * 4)
+
+	// precedance of operators
+	fmt.Println(3 + 4*4)
+	fmt.Println(3/3*4 - 4 + 2%2)
+
+}
