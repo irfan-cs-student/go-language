03a2f0a78271573ca5ee0940cf0da912ecf8aac6
M-irfan <fannee0011@gmail.com>
2026-07-12 17:27:43 +0500

    constants

---
+++ constants/constants.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+const (
+	name      = "irfan"
+	age       = 22
+	isStudent = true
+)
+
+func main() {
+
+	// const name = "irfan"
+	// const age = 22
+	// const isStudent = true
+
+	fmt.Println("name:", name, "age: ", age, "isStudent:", isStudent)
+
+}
