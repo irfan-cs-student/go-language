560d49c3292a54aaa3ae18abd9a136472690dfd6
M-irfan <fannee0011@gmail.com>
2026-07-12 17:41:39 +0500

    if-else

---
+++ if-else/if-else.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	name := "irfan"
+	isStudent := true
+
+	if isStudent != true {
+
+		fmt.Println("name:", name, "student:", isStudent)
+
+	} else {
+		fmt.Println(name, "not a srudent")
+	}
+}
