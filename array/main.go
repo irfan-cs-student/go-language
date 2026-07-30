29913c124c1a43966d4b85d2744ef59b882a7bec
M-irfan <fannee0011@gmail.com>
2026-07-30 10:48:54 +0500

    array

---
+++ array/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var num = [10]int{3, 2, 4, 4}
+	name := [7]string{"irfan", "saeed", "ali"}
+	fmt.Println("----With index------")
+	fmt.Println(name[1])
+	fmt.Println(num[2])
+
+	fmt.Println("----Without index------")
+	fmt.Println(name)
+	fmt.Println(num)
+
+	fmt.Println("---let practise-----")
+
+	abc := [...]int{4, 4, 2, 4, 2, 45, 23}
+	fmt.Println(abc)
+	fmt.Println(abc[1], abc[1])
+	fmt.Println("lenght of array", len(abc))
+	fmt.Println("addin index --", abc[0]+abc[0])
+
+}
